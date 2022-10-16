6062b3f5dd1aa3986e0d587b1d06c7972f084490
Victoria Konkiel <victoriakonkiel@Victorias-Air.hsd1.mn.comcast.net>
2022-10-16 00:43:21 -0500

    test servlet finally working

---
+++ FoundationalProject/src/main/java/com/revature/reimbursement/servlets/TestServlet.java
@@ -0,0 +1,23 @@
+package com.revature.reimbursement.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("[LOG] - TestServlet received a request at " + LocalDateTime.now());
+        System.out.println("[LOG] - Request URI: " + req.getRequestURI());
+        System.out.println("[LOG] - Request Method: " + req.getMethod());
+        System.out.println("[LOG] - Request Header, example " + req.getHeader("example"));
+
+        resp.setStatus(200);
+        resp.setHeader("Content-type", "text/plain");
+        resp.setHeader("example-response-header", "some-example-value");
+        resp.getWriter().write("This is the response payload");
+    }
+}
